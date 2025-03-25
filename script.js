4c86f16f00dc1c69c0b2e0025cd51afeefa1ac32
DavideCotigliani <davide.cotigliani@gmail.com>
2025-03-25 17:41:06 +0100

    [DONE]

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 // definisco le variabili e vado a prendere gli elementi che mi interessano
 const lampadina = document.getElementById('lampadina')
 const button = document.querySelector('button')
-
+// funzione per chiamare l'evento di click
 button.addEventListener('click', function (){
         lampadina.src= 'yellow_lamp.png';
     
